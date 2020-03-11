1e3447ac50dcf425aae96f4b933bc54edf27871f
170031410@kluniversity.omgit config --global  user.name 170031410@kluniversity.in <170031410@kluniversity.in>
2020-03-11 21:37:19 +0530

    commit

---
+++ src/week7/Singleton1.java
@@ -0,0 +1,14 @@
+package week7;
+
+public class Singleton1 {
+	public static void main(String[] args) {
+
+	      
+
+	      
+		   Singleton object = Singleton.getInstance();
+
+	     
+	      object.showMessage();
+	   }
+}
